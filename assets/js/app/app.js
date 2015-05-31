b29d84ab021252a8bbdc2a5ef3db9ebc32440a38
svillagracia <svillagracia@outlook.com>
2015-05-31 13:25:08 -0700

    Repair conflicts

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
 var Leviathan = angular.module('Leviathan',['ngRoute','ngResource','ngMaterial']);
-=======
-var Leviathan = angular.module('Leviathan',['ngRoute','ngResource']);
->>>>>>> 78c3776d68b2801f8abc25b6e01c21c01c259b66
 
 Leviathan.run([function(){
   console.log('Leviathan is running');
 }]);
 
-<<<<<<< HEAD
 Leviathan.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
 }]);
 
-=======
->>>>>>> 78c3776d68b2801f8abc25b6e01c21c01c259b66
 Leviathan.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvier){
   $locationProvier.html5Mode(true);
 
